d23ae9e13ecc21d7400be4afaed663a2d63ef374
Tyler Long <tyler4long@gmail.com>
2017-01-02 18:11:49 +0800

    First working version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-const taskListPlugin = (md, name) => {
+const taskListPlugin = (md) => {
+  md.tags = {}
+
+  md.renderer.renderInline = function (tokens, options, env) {
+    let result = md.renderer.constructor.prototype.renderInline.call(this, tokens, options, env)
+    if ((md.tags['bullet_list'] || 0) > 0 && (md.tags['list_item'] || 0) > 0) {
+      if (tokens[0].content.startsWith('[ ] ')) {
+        return '<input type="checkbox" disabled /> ' + result.substr(4)
+      } else if (tokens[0].content.startsWith('[x] ')) {
+        return '<input type="checkbox" disabled checked /> ' + result.substr(4)
+      }
+    }
+    return result
+  }
+
+  md.renderer.renderToken = function (tokens, idx, options) {
+    let token = tokens[idx]
+    let tag = token.type
+    if (tag.endsWith('_open')) {
+      let _tag = tag.substr(0, tag.length - 5)
+      md.tags[_tag] = (md.tags[_tag] || 0) + 1
+    } else if (tag.endsWith('_close')) {
+      let _tag = tag.substr(0, tag.length - 6)
+      md.tags[_tag] = (md.tags[_tag] || 0) - 1
+    }
+
+    if ((md.tags['bullet_list'] || 0) > 0 && tag === 'list_item_open' &&
+      (tokens[idx + 2].content.startsWith('[ ] ') || tokens[idx + 2].content.startsWith('[x] '))) {
+      token.attrPush(['class', 'task-list-item'])
+    }
+
+    return md.renderer.constructor.prototype.renderToken.call(this, tokens, idx, options)
+  }
 }
 
 export default taskListPlugin
--- test/index.js
+++ test/index.js
@@ -11,5 +11,5 @@ assert(mdi.render('Hello world').trim() === '<p>Hello world</p>', 'Hello world')
 console.log(mdi.render('- [ ] task 1'))
 console.log(mdi.render('- [x] task 2'))
 
-assert(mdi.render('- [ ] task 1').trim() === '<ul>\n<li>[ ] task 1</li>\n</ul>', '- [ ] task 1')
-assert(mdi.render('- [x] task 2').trim() === '<ul>\n<li>[x] task 2</li>\n</ul>', '- [x] task 2')
+assert(mdi.render('- [ ] task 1').trim() === '<ul>\n<li class="task-list-item"><input type="checkbox" disabled /> task 1</li>\n</ul>', '- [ ] task 1')
+assert(mdi.render('- [x] task 2').trim() === '<ul>\n<li class="task-list-item"><input type="checkbox" disabled checked /> task 2</li>\n</ul>', '- [x] task 2')
