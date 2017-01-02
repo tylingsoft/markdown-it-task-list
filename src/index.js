234bd0d8c6a63ebef2166435a2624f462f4963fb
Tyler Long <tyler4long@gmail.com>
2017-01-02 18:14:39 +0800

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const taskListPlugin = (md) => {
-  md.tags = {}
+  const tags = {}
 
   md.renderer.renderInline = function (tokens, options, env) {
     let result = md.renderer.constructor.prototype.renderInline.call(this, tokens, options, env)
-    if ((md.tags['bullet_list'] || 0) > 0 && (md.tags['list_item'] || 0) > 0) {
+    if ((tags['bullet_list'] || 0) > 0 && (tags['list_item'] || 0) > 0) {
       if (tokens[0].content.startsWith('[ ] ')) {
         return '<input type="checkbox" disabled /> ' + result.substr(4)
       } else if (tokens[0].content.startsWith('[x] ')) {
@@ -18,13 +18,13 @@ const taskListPlugin = (md) => {
     let tag = token.type
     if (tag.endsWith('_open')) {
       let _tag = tag.substr(0, tag.length - 5)
-      md.tags[_tag] = (md.tags[_tag] || 0) + 1
+      tags[_tag] = (tags[_tag] || 0) + 1
     } else if (tag.endsWith('_close')) {
       let _tag = tag.substr(0, tag.length - 6)
-      md.tags[_tag] = (md.tags[_tag] || 0) - 1
+      tags[_tag] = (tags[_tag] || 0) - 1
     }
 
-    if ((md.tags['bullet_list'] || 0) > 0 && tag === 'list_item_open' &&
+    if ((tags['bullet_list'] || 0) > 0 && tag === 'list_item_open' &&
       (tokens[idx + 2].content.startsWith('[ ] ') || tokens[idx + 2].content.startsWith('[x] '))) {
       token.attrPush(['class', 'task-list-item'])
     }
